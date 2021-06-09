73b7d4abada91c35fbe4e89226701943d83f2c02
vacingFang <vacingfang@tencent.com>
2021-06-09 21:30:31 +0800

    first edition of transcode compare tool

---
+++ score.py
@@ -0,0 +1,85 @@
+#encoding=utf-8
+import json
+import os
+
+import vutil
+
+exe = False
+def exe_cmd(cmd):
+    print(cmd)
+    if (not exe):
+        return
+
+    process = os.popen(cmd)
+    output = process.read()
+
+def get_main_name(val):
+    return "out_" + val;
+
+def get_json_name(ref, main):
+    return ref + "_" + main;
+
+def load_config(conf_path):
+    with open(conf_path, 'r') as conf_file:
+        conf = json.load(conf_file)
+    if (not "test" in conf or
+        not "x264" in conf):
+        print("section test or x264 not in configure file")
+    return conf
+
+eval_cmd_patern = 'ffmpeg -i {main} -s:v {ref_dim} -i {ref} -filter_complex \
+[0:v][1:v]libvmaf=psnr=1:ssim=1:phone_model=0:log_path={log_path}:log_fmt=json \
+-f null -'
+x264_cmd_patern = "{x264_bin} {in_par} {comm_par} {test_par} {test_val} -o {out} {in_file}"
+out_dir = "out/"
+log_dir = out_dir + "log/"
+vutil.make_dir(out_dir)
+vutil.make_dir(log_dir)
+
+if __name__ == "__main__":
+    out_files = []
+    conf = load_config("conf.json")
+    conf_test = conf["test"]
+    ref_file = conf_test["ref"]["file"]
+    [_, ref_name, _] = vutil.sep_path_segs(ref_file)
+
+    conf_x264 = conf["x264"]
+    for val in conf_x264["test_value"]:
+        main_file = out_dir + get_main_name(val) + ".264"
+        x264_cmd = x264_cmd_patern.format(
+                x264_bin=conf_x264["bin_path"],
+                comm_par=conf_x264["comm_par"],
+                test_par=conf_x264["test_par"],
+                in_file=conf["test"]["ref"]["file"],
+                in_par="--input-res " + conf["test"]["ref"]["dim"],
+                test_val=val,
+                out=main_file
+                )
+        exe_cmd(x264_cmd)
+        out_files.append(main_file)
+
+    for main_file in out_files:
+        [_, main_name, _] = vutil.sep_path_segs(main_file)
+        cmd = eval_cmd_patern.format(
+                    main = main_file,
+                    ref = ref_file,
+                    ref_dim="1920x1080", 
+                    log_path=log_dir + get_json_name(ref_name, main_name) + ".json"
+                    )
+        exe_cmd(cmd)
+
+    scores = {}
+    for val in conf_x264["test_value"]:
+        main = get_main_name(val)
+        main_file = out_dir + main + ".264"
+        json_path = log_dir + get_json_name(ref_name, main) + ".json"
+        with open(json_path, 'r') as score_f:
+            score = json.load(score_f)
+            scores[val] = {
+                    "vmaf": round(score["VMAF score"], 5),
+                    "psnr": round(score["PSNR score"], 5),
+                    "ssim": round(score["SSIM score"], 5),
+                    "size": os.path.getsize(main_file)
+                }
+            print(scores[val])
+
---
+++ vutil.py
@@ -0,0 +1,199 @@
+# encoding=utf-8
+import logging
+import sys
+import os
+import shutil
+import glob
+import logging
+import random
+import chardet
+
+def pretty_args_str(args):
+    args_str = ''
+    for arg in vars(args):
+        args_str += '\n\t%s:\t[%s]' % (arg, repr(getattr(args, arg)))
+
+    return args_str
+
+def init_logger(fn=None):
+    from imp import reload
+    reload(logging)
+
+    logging_params = { 
+        'level': logging.INFO,
+        'format': '%(asctime)s__[%(levelname)s, %(module)s.%(funcName)s](%(name)s)__[L%(lineno)d] %(message)s',
+    }   
+
+    if fn is not None:
+        logging_params['filename'] = fn
+
+    logging.basicConfig(**logging_params)
+    logging.debug('init basic configure of logging success')
+
+
+def make_dir(p, del_old=False):
+    if os.path.exists(p):  # 文件夹存在
+        if del_old:
+            shutil.rmtree(p)        # 删除旧文件夹
+    else:
+        try:
+            os.mkdir(p)  # 创建文件夹
+        except FileNotFoundError:
+            os.makedirs(p, exist_ok=True)
+
+
+def sep_path_segs(path):
+    filepath, tempfilename= os.path.split(path)
+    shortname, extension = os.path.splitext(tempfilename)
+    return filepath, shortname, extension
+
+def copy_file_noexcept(src, dst, force=True):
+    ret = False
+    try:
+        # 文件已存在，且非强制拷贝
+        if not force and os.path.exists(dst):
+            pass
+        elif os.path.exists(dst) and os.path.samefile(src, dst):
+            logging.info('copy source and dst are the same file [%s]' % src)
+        else:
+            shutil.copyfile(src, dst)
+            ret = True
+    except:
+        logging.error(sys.exc_info()[1])
+
+    return ret
+
+
+def move_file_noexcept(src, dst, force=True):
+    ret = False
+    try:
+        # 文件已存在，且非强制移动
+        if not force and os.path.exists(dst):
+            pass
+        elif os.path.exists(dst) and os.path.samefile(src, dst):
+            logging.info('source and dst are the same file [%s]' % src)
+        else:
+            shutil.move(src, dst)
+            ret = True
+    except:
+        logging.error(sys.exc_info()[1])
+
+    return ret
+
+
+def multi_copy_files(src_file, dst_dir, up_times, _copy_suff='_copy_', force_copy=False):
+    if not 0 < up_times < 20:
+        return -1
+        logging.error('upsample time error, up_times=%d' % up_times)
+
+    _, fn, suf = sep_path_segs(src_file)
+    if not force_copy and fn.endswith(_copy_suff):
+        logging.warning('[%s] is already a copy, ignored' % src_file)
+        return 0
+
+    real_cpy_cnt = 0
+    for cnt in range(up_times):
+        if cnt == 0:
+            file_cnt = fn + suf
+        else:
+            # rand_num = random.randint(0, 1e4)
+            rand_num = cnt
+            file_cnt = fn + '_' + '{:04d}'.format(rand_num) + _copy_suff + suf
+
+        checked_file_cpy = os.path.join(dst_dir, file_cnt)
+        cpy_ret = copy_file_noexcept(src_file, checked_file_cpy, force=False)
+        if cpy_ret:
+            real_cpy_cnt += 1
+
+    return real_cpy_cnt
+
+
+img_suffixes = ['.jpg', '.jpeg', '.bmp', '.png']
+def get_files_recursively(dir_path, suffixes, level=1):
+    '''
+    递归读取目录下指定后缀的文件。
+    默认目录结构
+    d1 -\
+        |_x.img             # level 1
+        |_ ...
+        |_ d11 -\
+            |_x.img         # level 2
+            |_ ...
+            |_ d111 -\
+                |_x.img     # level 3
+                |_ ...
+    :param dir_path:
+    :param suffixes:
+    :param level:
+    :return:
+    '''
+    if len(suffixes) == 0:
+        raise ValueError('suffixes length is 0')
+
+    # level = 1: dir_path/*.jpg
+    # level = 3: dir_path/*/*/*.jpg
+    if level < 1 or level > 4:
+        raise ValueError('level must between [1, 3], but input=%d' % level)
+
+    files = []
+    for file_suffix in suffixes:
+        path_preffix = dir_path
+        for i in range(level):
+            path_preffix = os.path.join(path_preffix, '*')
+            # print(path_preffix)
+            files.extend(glob.glob(path_preffix + file_suffix))
+    
+    return files
+
+
+def find_files_recursively(file_name, dir_path, level=1):
+    '''
+    递归一个文件夹及其子文件夹，搜索指定文件
+    '''
+    # level = 1: dir_path/*.jpg
+    # level = 3: dir_path/*/*/*.jpg
+    if level < 1 or level > 4:
+        raise ValueError('level must between [1, 3], but input=%d' % level)
+
+    _, fn, suff = sep_path_segs(file_name)
+    file_name_raw = fn + suff
+
+    files = []
+    path_preffix = dir_path
+    for i in range(level):
+        # full file name, don't need to add * before it
+        files.extend(glob.glob(path_preffix + file_name_raw))
+
+        path_preffix = os.path.join(path_preffix, '*')
+        # print(path_preffix)
+    
+    return files
+
+
+def get_encd(f_path):
+    assert os.path.exists(f_path), 'file not exists[%s]' % f_path
+    assert os.path.isfile(f_path), 'not file[%s]' % f_path
+
+    with open(f_path, 'rb') as f:
+        data = f.read()
+        res = chardet.detect(data)
+
+    ret_enc = res['encoding'].lower()
+    if ret_enc == 'gb2312':
+        ret_enc = 'gbk'
+    logging.debug("[%s] encoding is [%s(%s)]" % (f_path, ret_enc, res['encoding']))
+
+    return ret_enc
+
+if __name__ == '__main__':
+    # path = '../'
+    # # print(sep_path_segs('./a/b c/de f.txt'))
+    # # print(sep_path_segs(r'.\a\b c\de f.txt'))
+    # # make_dir(r'D:\DataTemp\game_type_new\p2p\csgo_dst\\')
+    # files = find_files_recursively('util.py', path, level=2)
+    # print(files)
+
+    init_logger()
+    logging.error(2)
+    logging.getLogger().setLevel(logging.DEBUG)
+
